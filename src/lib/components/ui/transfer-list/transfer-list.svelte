5641e5864e1d04596fdd5e2fa49ffeea3db327b4
Sebastian Drozd <163430081+tzezar@users.noreply.github.com>
2024-08-15 19:26:25 +0200

    Initial commit

---
+++ src/lib/components/ui/transfer-list/transfer-list.svelte
@@ -0,0 +1,173 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import Button from '../button/button.svelte';
+	import Input from '../input/input.svelte';
+	import Checkbox from '../checkbox/checkbox.svelte';
+	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
+
+	type Item = {
+		id: number;
+		[key: string]: unknown;
+	};
+
+	// Main Picklist props
+	export let source: Item[] = [];
+	export let target: Item[] = [];
+	export let sourceHeader = '';
+	export let targetHeader = '';
+	export let filterBy = '';
+
+	// Internal state
+	let sourceSelection: Item[] = [];
+	let targetSelection: Item[] = [];
+	let sourceFilterValue = '';
+	let targetFilterValue = '';
+
+	const dispatch = createEventDispatcher();
+
+	// Filtering logic
+	$: filteredSource =
+		filterBy && sourceFilterValue
+			? source.filter((item) =>
+					String(item[filterBy]).toLowerCase().includes(sourceFilterValue.toLowerCase())
+				)
+			: source;
+
+	$: filteredTarget =
+		filterBy && targetFilterValue
+			? target.filter((item) =>
+					String(item[filterBy]).toLowerCase().includes(targetFilterValue.toLowerCase())
+				)
+			: target;
+
+	// Event handlers
+	function onTransfer(newSource: Item[], newTarget: Item[]) {
+		source = newSource;
+		target = newTarget;
+		sourceSelection = [];
+		targetSelection = [];
+		dispatchChange();
+	}
+
+	function dispatchChange() {
+		dispatch('change', { source, target });
+	}
+
+	// Selection logic
+	function onSelectionChange(list: any, selection: Item[], item: Item) {
+		const index = selection.indexOf(item);
+		if (index > -1) {
+			return selection.filter((_, i) => i !== index);
+		} else {
+			return [...selection, item];
+		}
+	}
+
+	function onSourceSelectionChange(item: Item) {
+		sourceSelection = onSelectionChange(source, sourceSelection, item);
+	}
+
+	function onTargetSelectionChange(item: Item) {
+		targetSelection = onSelectionChange(target, targetSelection, item);
+	}
+
+	// Transfer logic
+	function moveToTarget() {
+		const newSource = source.filter((item) => !sourceSelection.includes(item));
+		const newTarget = [...target, ...sourceSelection];
+		onTransfer(newSource, newTarget);
+	}
+
+	function moveToSource() {
+		const newTarget = target.filter((item) => !targetSelection.includes(item));
+		const newSource = [...source, ...targetSelection];
+		onTransfer(newSource, newTarget);
+	}
+
+	function moveAllToTarget() {
+		onTransfer([], [...target, ...source]);
+	}
+
+	function moveAllToSource() {
+		onTransfer([...source, ...target], []);
+	}
+</script>
+
+<div class="flex flex-col gap-4 md:flex-row">
+	<div class="w-full">
+		{#if sourceHeader}
+			<div class="pb-2">{sourceHeader}</div>
+		{/if}
+
+		{#if filterBy}
+			<div class="flex flex-row">
+				<Input
+					type="text"
+					class="rounded-b-none"
+					placeholder="Filter"
+					bind:value={sourceFilterValue}
+				/>
+				<div class="flex flex-row">
+					<Button class="w-10 rounded-none" on:click={moveToTarget}>&gt;</Button>
+					<Button class="w-10 rounded-none rounded-tr-md" on:click={moveAllToTarget}
+						>&gt;&gt;</Button
+					>
+				</div>
+			</div>
+		{/if}
+
+		<ul class="flex h-[140px] flex-col rounded-b-md border-[1px] border-solid border-input p-2 ">
+			<ScrollArea>
+				{#each filteredSource as item (item.id)}
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<!-- svelte-ignore a11y-no-static-element-interactions -->
+					<div
+						class="flex flex-row items-center gap-x-2 rounded-md px-1 py-1 hover:bg-secondary mr-4"
+						on:click={() => onSourceSelectionChange(item)}
+					>
+						<Checkbox class='self-start mt-1' checked={sourceSelection.includes(item)} />
+						<li class="w-full">
+							{item[filterBy]}
+						</li>
+					</div>
+				{/each}
+			</ScrollArea>
+		</ul>
+	</div>
+	<div class="w-full">
+		{#if targetHeader}
+			<div class="pb-2">{targetHeader}</div>
+		{/if}
+
+		{#if filterBy}
+			<div class="flex flex-row">
+				<Button class="w-10 rounded-none rounded-tl-md" on:click={moveAllToSource}>&lt;&lt;</Button>
+				<Button class="w-10 rounded-none" on:click={moveToSource}>&lt;</Button>
+				<Input
+					type="text"
+					class="p-picklist-filter"
+					placeholder="Filter"
+					bind:value={targetFilterValue}
+				/>
+			</div>
+		{/if}
+
+		<ul class="flex h-[140px] flex-col rounded-b-md border-[1px] border-solid border-input p-2">
+			<ScrollArea>
+				{#each filteredTarget as item (item.id)}
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<!-- svelte-ignore a11y-no-static-element-interactions -->
+					<div
+						class="flex flex-row items-center gap-x-2 rounded-md px-1 py-1 hover:bg-secondary pr-4"
+						on:click={() => onTargetSelectionChange(item)}
+					>
+						<Checkbox class='self-start mt-1' checked={targetSelection.includes(item)} />
+						<li class="w-full">
+							{item[filterBy]}
+						</li>
+					</div>
+				{/each}
+			</ScrollArea>
+		</ul>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,141 @@
+<script lang="ts">
+	import TransferList from '$lib/components/ui/transfer-list/transfer-list.svelte';
+	import * as Card from '$lib/components/ui/card';
+	import ScrollArea from '$lib/components/ui/scroll-area/scroll-area.svelte';
+	import Highlight from 'svelte-highlight';
+	import typescript from 'svelte-highlight/languages/typescript';
+	import atomOneDark from 'svelte-highlight/styles/atom-one-dark';
+	import { mode } from 'mode-watcher';
+	import logoBlack from '$lib/assets/tzezar-logo-black.png';
+	import logoWhite from '$lib/assets/tzezar-logo-white.png';
+
+	let source: { id: number; name: string }[] = [
+		{ id: 1, name: 'Kingdom of Heaven 2005' },
+		{ id: 2, name: 'The Lord of the Rings: The Fellowship of the Ring 2001' },
+		{ id: 3, name: 'The Shawshank Redemption 1994' },
+		{ id: 4, name: 'Forrest Gump 1994' },
+		{ id: 5, name: 'Se7en 1995' }
+	];
+	let target: typeof source = [];
+
+	function onChange(event: CustomEvent) {
+		source = event.detail.source;
+		target = event.detail.target;
+	}
+
+	let code1 = `<scriptTag lang='ts'> // I could not close "script" tag in markdown for svelte reasons. Remember to rename it.
+    import TransferList from '$lib/components/ui/transfer-list/transfer-list.svelte';
+    
+    // in current version shape of source object should be { id: number; name: string }, you can change it for your needs in the component
+    let source: { id: number; name: string }[] = [
+        { id: 1, name: 'Kingdom of Heaven 2005' },
+        { id: 2, name: 'The Lord of the Rings: The Fellowship of the Ring 2001' },
+        { id: 3, name: 'The Shawshank Redemption 1994' },
+        { id: 4, name: 'Forrest Gump 1994' },
+        { id: 5, name: 'Se7en 1995' }
+    ];
+    let target: typeof source = [];
+
+    function onChange(event: CustomEvent) {
+        source = event.detail.source;
+        target = event.detail.target;
+    }
+</scriptTag> 
+
+<TransferList
+    bind:source
+    bind:target
+    sourceHeader="Movies"
+    targetHeader="Favourite Movies"
+    filterBy="name"
+    on:change={onChange}
+/>
+`;
+</script>
+
+<svelte:head>
+	{@html atomOneDark}
+</svelte:head>
+<div class="flex flex-col gap-4">
+	<div class='flex flex-row gap-4'>
+		{#if $mode == 'dark'}
+		<img src={logoWhite} alt="" srcset="" class="h-[64px]" />
+		{:else}
+		<img src={logoBlack} alt="" srcset="" class="h-[64px]" />
+		<!-- else content here -->
+		{/if}
+		<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+
+			shadcn-svelte transfer-list
+		</h1>
+	</div>
+	<h2
+		class="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+	>
+		Showcase
+	</h2>
+	<TransferList
+		bind:source
+		bind:target
+		sourceHeader="Movies"
+		targetHeader="Favourite Movies"
+		filterBy="name"
+		on:change={onChange}
+	/>
+	<div class="flex flex-wrap gap-4 md:flex-nowrap">
+		<Card.Root class="w-full md:w-1/2">
+			<Card.Header>
+				<Card.Title>Movies</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<ScrollArea>
+					<code>
+						{JSON.stringify(source, null, 2)}
+					</code>
+				</ScrollArea>
+			</Card.Content>
+		</Card.Root>
+		<Card.Root class="w-full md:w-1/2">
+			<Card.Header>
+				<Card.Title>Favourite Movies</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<code class="">
+					{JSON.stringify(target, null, 2)}
+				</code>
+			</Card.Content>
+		</Card.Root>
+	</div>
+	<div class="flex flex-col gap-4">
+		<h2
+			class="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+		>
+			Usage
+		</h2>
+
+		<p>
+			1. Copy component from <a
+				class="font-medium text-primary underline underline-offset-4"
+				href="https://github.com/tzezar/shadcn-svelte-transfer-list/blob/main/src/lib/components/ui/transfer-list/transfer-list.svelte"
+				>GITHUB REPOSITORY</a
+			> and paste it into your components folder.
+		</p>
+		<p>2. Use the component in your Svelte file as shown below:</p>
+		<Highlight language={typescript} code={code1} />
+		<p class="text-sm text-muted-foreground">
+			Remember to add shadcn-svelte Button, Checkbox, ScrollArea, Input components.
+		</p>
+	</div>
+	<p class="pt-4">
+		Thanks to <a
+			class="font-medium text-primary underline underline-offset-4"
+			href="https://khromov.se/">khromov</a
+		> and Claude for creating the base svelte version.
+	</p>
+	<p class="pb-6 italic">
+		If you find the project interesting feel free to give a star on <a
+		class="font-medium text-primary underline underline-offset-4"
+			href="https://github.com/tzezar/shadcn-svelte-transfer-list">github</a
+		>. Thanks! ❤️
+	</p>
+</div>
